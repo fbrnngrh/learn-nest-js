084bfdb4da4301e35743818363dcc34eacd0ba1a
luckyde933 <luckydev933@gmail.com>
2023-10-13 09:49:04 +0800

    learn request body and DTO

--- src/users/controller/users/users.controller.ts
+++ src/users/controller/users/users.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { CreateUserDto } from 'src/users/dto/CreateUserData.dto';
 
 @Controller('users')
 export class UsersController {
@@ -9,5 +11,11 @@ export class UsersController {
       email: 'john@gmail,com',
       password: '123456',
     };
+  }
+
+  @Post()
+  createUser(@Body() usersData: CreateUserDto) {
+    console.log(usersData);
+    return 'ok';
   }
 }
